554b951fb16e6ba35d906b3eff226378cfc14f24
Merinorus <merinorus.antoine@gmail.com>
2016-10-12 12:39:06 +0200

    Created empty notebooks
    One for each exercise. They're ready to be filled !

---
+++ Homework/02 - Data from the Web/Question 1.py
@@ -0,0 +1,9 @@
+
+# coding: utf-8
+
+# Obtain all the data for the Bachelor students, starting from 2007. Keep only the students for which you have an entry for both Bachelor semestre 1 and Bachelor semestre 6. Compute how many months it took each student to go from the first to the sixth semester. Partition the data between male and female students, and compute the average -- is the difference in average statistically significant?
+
+# In[ ]:
+
+
+
