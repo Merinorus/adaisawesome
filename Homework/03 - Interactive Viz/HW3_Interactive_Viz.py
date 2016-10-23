9a6222b9cf95f2f87a429d8c25fd8f490a2bede6
merinorus <merinorus.antoine@gmail.com>
2016-10-23 11:01:45 +0200

    Wrangled csv file in pandas

--- Homework/03 - Interactive Viz/HW3_Interactive_Viz.py
+++ Homework/03 - Interactive Viz/HW3_Interactive_Viz.py
@@ -4,39 +4,49 @@
 # Build a Choropleth map which shows intuitively (i.e., use colors wisely) how much grant money goes to each Swiss canton.
 # Good luck
 
-# In[2]:
+# In[61]:
 
 import pandas as pd
 import numpy as np
 
 
-# In[22]:
+# In[62]:
 
 p3_grant_export_data = pd.read_csv("P3_GrantExport.csv", sep=";")
 p3_grant_export_data
 
 
-# In[23]:
+# In[63]:
 
-p3_grant_export_data = p3_grant_export_data[p3_grant_export_data['Approved Amount'].apply(lambda x : x.isnumeric())]
+p3_grant_export_data.size
 
 
-# In[24]:
+# In[64]:
 
+# We keep only the rows which mention how much money has been granted (the amount column starts by a number)
+p3_grant_export_data = p3_grant_export_data[p3_grant_export_data['Approved Amount'].apply(lambda x : x[0].isdigit())]
+
+
+# In[65]:
+
+# Almost 200k rows have been removed
+p3_grant_export_data.size
+
+
+# In[67]:
+
+# We don't need this data
+p3_grant_export_data = p3_grant_export_data.drop(p3_grant_export_data.columns[[0]], axis = 1)
+p3_grant_export_data = p3_grant_export_data.drop(['Project Title', 'Project Title English', 'Responsible Applicant', 'Discipline Number', 'Discipline Name Hierarchy', 'Keywords'], axis=1)
 p3_grant_export_data
-
-
-# In[ ]:
-
-df[df.id.apply(lambda x: x.isnumeric())]
 
 
 # In[28]:
 
-str.isnumeric('200.92')
+
 
 
 # In[27]:
 
-str.isnumeric('200')
+
 
