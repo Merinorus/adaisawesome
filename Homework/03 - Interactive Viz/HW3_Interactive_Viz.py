266677e0b1974b14a29838e089c6ff5994d1682a
merinorus <merinorus.antoine@gmail.com>
2016-10-23 11:37:24 +0200

    Wrangled data
    Universities, approved amount is numeric

--- Homework/03 - Interactive Viz/HW3_Interactive_Viz.py
+++ Homework/03 - Interactive Viz/HW3_Interactive_Viz.py
@@ -3,36 +3,36 @@
 
 # Build a Choropleth map which shows intuitively (i.e., use colors wisely) how much grant money goes to each Swiss canton.
 
-# In[81]:
+# In[105]:
 
 import pandas as pd
 import numpy as np
 
 
-# In[82]:
+# In[106]:
 
 p3_grant_export_data = pd.read_csv("P3_GrantExport.csv", sep=";")
 p3_grant_export_data
 
 
-# In[83]:
+# In[107]:
 
 p3_grant_export_data.size
 
 
-# In[84]:
+# In[108]:
 
 # We keep only the rows which mention how much money has been granted (the amount column starts by a number)
 p3_grant_export_data = p3_grant_export_data[p3_grant_export_data['Approved Amount'].apply(lambda x : x[0].isdigit())]
 
 
-# In[85]:
+# In[109]:
 
 # Almost 200k rows have been removed
 p3_grant_export_data.size
 
 
-# In[86]:
+# In[110]:
 
 # We don't need this data
 p3_grant_export_data = p3_grant_export_data.drop(p3_grant_export_data.columns[[0]], axis = 1)
@@ -44,21 +44,46 @@ p3_grant_export_data.size
 # We will ignore all project in which the University is not mentioned : we assume that if it's not, the project is probably outside Switzerland.
 # If we have the time, a better solution would be taking the institution's location into account as well.
 
-# In[80]:
+# In[111]:
 
 # Removing rows in which University is not mentioned
 p3_grant_export_data = p3_grant_export_data.dropna(subset=['University'])
 p3_grant_export_data.size
 
 
-# In[87]:
+# In[112]:
 
 p3_grant_export_data
 
 
-# In[91]:
+# In[104]:
 
 # We also delete every row that contains "Nicht zuteilbar - NA", which means that University is not mentioned.
 p3_grant_export_data = p3_grant_export_data[p3_grant_export_data.University.str.contains('Nicht zuteilbar - NA') == False]
 p3_grant_export_data.head()
+
+
+# In[119]:
+
+# The 'Approved Amount' column contains string types instead of numbers
+type(p3_grant_export_data['Approved Amount'][0])
+
+
+# In[127]:
+
+# Let's convert this column to float numbers, so we'll be able to do some maths
+p3_grant_export_data['Approved Amount'] = p3_grant_export_data['Approved Amount'].apply(float)
+
+
+# In[128]:
+
+# Now we definitely have numbers in the 'Approved Amount' column !
+type(p3_grant_export_data['Approved Amount'][0])
+
+
+# Now, time to locate universities...
+
+# In[ ]:
+
+
 
