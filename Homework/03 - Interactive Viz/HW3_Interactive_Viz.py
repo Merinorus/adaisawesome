8765a2f24e1cc534dc7226f585322abe87dc55e8
merinorus <merinorus.antoine@gmail.com>
2016-10-23 11:21:06 +0200

    Wrangled data a little bit more

--- Homework/03 - Interactive Viz/HW3_Interactive_Viz.py
+++ Homework/03 - Interactive Viz/HW3_Interactive_Viz.py
@@ -2,51 +2,63 @@
 # coding: utf-8
 
 # Build a Choropleth map which shows intuitively (i.e., use colors wisely) how much grant money goes to each Swiss canton.
-# Good luck
 
-# In[61]:
+# In[81]:
 
 import pandas as pd
 import numpy as np
 
 
-# In[62]:
+# In[82]:
 
 p3_grant_export_data = pd.read_csv("P3_GrantExport.csv", sep=";")
 p3_grant_export_data
 
 
-# In[63]:
+# In[83]:
 
 p3_grant_export_data.size
 
 
-# In[64]:
+# In[84]:
 
 # We keep only the rows which mention how much money has been granted (the amount column starts by a number)
 p3_grant_export_data = p3_grant_export_data[p3_grant_export_data['Approved Amount'].apply(lambda x : x[0].isdigit())]
 
 
-# In[65]:
+# In[85]:
 
 # Almost 200k rows have been removed
 p3_grant_export_data.size
 
 
-# In[67]:
+# In[86]:
 
 # We don't need this data
 p3_grant_export_data = p3_grant_export_data.drop(p3_grant_export_data.columns[[0]], axis = 1)
-p3_grant_export_data = p3_grant_export_data.drop(['Project Title', 'Project Title English', 'Responsible Applicant', 'Discipline Number', 'Discipline Name Hierarchy', 'Keywords'], axis=1)
+p3_grant_export_data = p3_grant_export_data.drop(['Project Title', 'Project Title English', 'Responsible Applicant', 'Discipline Number', 'Discipline Name', 'Discipline Name Hierarchy', 'Keywords'], axis=1)
+p3_grant_export_data.size
+
+
+# First, we will locate projcets according to the University name.
+# We will ignore all project in which the University is not mentioned : we assume that if it's not, the project is probably outside Switzerland.
+# If we have the time, a better solution would be taking the institution's location into account as well.
+
+# In[80]:
+
+# Removing rows in which University is not mentioned
+p3_grant_export_data = p3_grant_export_data.dropna(subset=['University'])
+p3_grant_export_data.size
+
+
+# In[87]:
+
 p3_grant_export_data
 
 
-# In[28]:
+# In[91]:
 
-
-
-
-# In[27]:
-
-
+# We also delete every row that contains "Nicht zuteilbar - NA", which means that University is not mentioned.
+p3_grant_export_data = p3_grant_export_data[p3_grant_export_data.University.str.contains('Nicht zuteilbar - NA') == False]
+p3_grant_export_data.head()
 
