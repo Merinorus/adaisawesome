48ecfefcdfd225bd3f0773a19c0f88b5537833d8
merinorus <merinorus.antoine@gmail.com>
2016-10-25 23:13:53 +0200

    Trying a different way to get cantons
    Ongoing...

--- Homework/03 - Interactive Viz/HW3_Interactive_Viz.py
+++ Homework/03 - Interactive Viz/HW3_Interactive_Viz.py
@@ -64,21 +64,18 @@ p3_grant_export_data
 # Using only university names as a parametrer for geolocators isn't enough, because we get about half of the results.
 # A better idea would be using university name, and if there is no result, using the institution name as a second chance.  
 # We will then take both universities and institution names into account in order to do our research:
-# 1) Create a key-value (name-canton) dictionary for universities and institutions :
-#     ['University', 'Canton'] and ['Institution', 'Canton']
+# 1) Create initial data containers :
+#     - a key-value (name-canton) dictionary for universities and institutions :
+#         -['University', 'Canton'] and ['Institution', 'Canton']
+#     - a table that contains all cantons that have been found
 # 2) Go trough the dataframe:
 #   - Check if the university name exists in our index. If not, geolocate the address 
-#   - If several addresses are returned, check one that is in Switzerland (eg: University of Geneva uses to be found is US !)
-#   - If the university address is not relevant, try to find it with institution name, the same way as above
+#   - Check if the address is in Switzerland, otherwise canton will be considered as 'None'
+#   - If the university address is not found, try to find it with institution name, the same way as above
 #   - Extract the canton of the address (if it found an address and if it's in Switzerland). If no canton, let's say 'None' canton
 #   - Add the canton name to a the dictionary (or add something like 'None' if no canton has been found), so next university or institution that has been already found won't have to be geolocated again
-#   - Add the canton to a table
+#   - Add the canton to the canton table
 # 3) Add the canton table to the above dataframe in a way that they match with the universities or institutions
-
-# In[ ]:
-
-
-
 
 # In[66]:
 
@@ -89,14 +86,87 @@ googlemapsapikey = googlemapsapikeyjson['key']
 
 # In[68]:
 
-#geolocator = geopy.geocoders.GeoNames(None, geonames_login)
 geolocator = geopy.geocoders.GoogleV3(api_key=googlemapsapikey)
+# Do a test with University of Geneva
 test_university_geneva = geolocator.geocode("University of Geneva")
 test_university_geneva
 
 
-# That's why we have to be careful and check if an address is relevant to Switzerland !
-# So let's start by creating the indexes for 
+# University of Geneva appears to be in USA ! But the one of our Dataframe is without a doubt the Swiss one.
+
+# In[92]:
+
+# Specifying the region for the geolocator
+test_university_geneva = geolocator.geocode("University of Geneva", region='ch')
+test_university_geneva
+
+
+# Oh oh, that's much better !
+# Now let's start by creating the indexes for universities and institutions :
+
+# In[83]:
+
+university_canton_dict = {}
+institution_canton_dict = {}
+# We can already add the values in our dataframe that won't lead to an address
+university_canton_dict['Nicht zuteilbar - NA'] = None # it means "Not Available" in German !
+institution_canton_dict['NaN'] = None
+
+
+# Then we create tables that will contains every canton we find, so we'll be able to match it with the dataframe at the end.
+
+# In[90]:
+
+canton_shortname_table = [] # eg: VD
+canton_longname_table = []# eg: Vaud
+
+
+# In[89]:
+
+limit = 1
+counter = 0
+# Go through the dataframe
+for index, row in p3_grant_export_data.iterrows():
+    # Check if the university name exists in our index
+    university_name = row['University']
+    if university_name in university_canton_dict:
+        # The university has already been found. Let's add the canton to the canton table
+        canton_table.append(university_canton_dict[university_name])
+    else:
+        # The university has not been found, so we have to geolocate it
+        adr = geolocator.geocode(university_name, region='ch')
+        if adr is None:
+            # TODO No address has been found for this University. So we have to do the same with Institution
+            institution_name = row['Institution']
+            adr = geolocator.geocode(institution_name, region='ch')
+        # Now, address should have been found, either by locating the university or the institution
+        if adr is None:
+            # No address has been found for the institution either, so we give up with this row
+        else:
+            # TODO check if it's a Swiss address, if yes add the canton to the table
+            try:
+                for i in adr.raw['address_components']:
+                    if i["types"][0] == "administrative_area_level_1":
+                        canton_longname = (i['long_name'])
+            except KeyError:
+                print('No canton found')
+            
+            
+
+    counter = counter + 1
+    if counter >= limit:
+        break
+
+
+# In[77]:
+
+
+
+
+# In[84]:
+
+university_canton_dict['Nicht zuteilbar - NA']
+
 
 # In[ ]:
 
--- Homework/03 - Interactive Viz/folium_test.py
+++ Homework/03 - Interactive Viz/folium_test.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[21]:
+# In[1]:
 
 import folium
 map_osm = folium.Map(location=[45.5236, 44])
@@ -14,25 +14,80 @@ map_osm.save("cool_place.html")
 
 
 
-# In[8]:
+# In[2]:
 
 map_osm = folium.Map(location=[45.5236, 44], tiles='Stamen Terrain')
 map_osm
 
 
-# In[9]:
+# In[3]:
 
 map_osm = folium.Map(location=[45.5236, 44], tiles='Stamen Toner')
 map_osm
 
 
-# In[10]:
+# In[4]:
 
 map_osm = folium.Map(location=[45.5236, 44], tiles='Mapbox Bright')
 map_osm
 
 
+# In[5]:
+
+import json
+import geopy
+googlemapsapikeyjson = json.loads(open('google_maps_api_key.json').read())
+googlemapsapikey = googlemapsapikeyjson['key']
+geolocator = geopy.geocoders.GoogleV3(api_key=googlemapsapikey)
+adr = geolocator.geocode("University of Geneva", region='ch')
+
+
+# In[6]:
+
+adr.raw
+
+
+# In[7]:
+
+# displays the dictionary in a visually better way
+print(json.dumps(adr.raw, indent = 4))
+
+
+# In[8]:
+
+adr.raw['address_components']
+
+
+# In[14]:
+
+canton_longname = None
+try:
+    for i in adr.raw['address_components']:
+        if i["types"][0] == "administrative_area_level_1":
+            canton_longname = (i['long_name'])
+except KeyError:
+    print('No canton found')
+canton_longname
+
+
+# In[12]:
+
+adr.raw['address_componentds']
+
+
+# In[10]:
+
+"administrative_area_level_1" in adr.raw['address_components'][*][0]
+
+
 # In[ ]:
 
-map_ch = 
+null_adr =  geolocator.geocode("azdadefqefesrhtrtdgefregesrg", region='ch')
+type(null_adr)
+
+
+# In[ ]:
+
+if null_adr is None:
+    print("Nonetype !")
 
