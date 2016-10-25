2a1f031167e6a066e9938d66cb57130def5b1876
merinorus <merinorus.antoine@gmail.com>
2016-10-25 21:57:00 +0200

    Trying a different way to find cantons
    Using only university names as a parametrer for geolocators isn't
    enough, because we get about half of the results.
    A better idea would be using university name, and if there is no result,
    using the institution name as a second chance.
    We will then take both universities and institution names into account
    in order to do our research:
    1) Create a key-value (name-canton) dictionary for universities and
    institutions :
    ['University', 'Canton'] and ['Institution', 'Canton']
    2) Go trough the dataframe:
    - Check if the university name exists in our index. If not, geolocate
    the address
    - If several addresses are returned, check one that is in Switzerland
    (eg: University of Geneva uses to be found is US !)
    - If the university address is not relevant, try to find it with
    institution name, the same way as above
    - Extract the canton of the address (if it found an address and if it's
    in Switzerland). If no canton, let's say 'None' canton
    - Add the canton name to a the dictionary (or add something like 'None'
    if no canton has been found), so next university or institution that has
    been already found won't have to be geolocated again
    - Add the canton to a table
    3) Add the canton table to the above dataframe in a way that they match
    with the universities or institutions

--- Homework/03 - Interactive Viz/HW3_Interactive_Viz.py
+++ Homework/03 - Interactive Viz/HW3_Interactive_Viz.py
@@ -3,7 +3,7 @@
 
 # Build a Choropleth map which shows intuitively (i.e., use colors wisely) how much grant money goes to each Swiss canton.
 
-# In[135]:
+# In[54]:
 
 import pandas as pd
 import numpy as np
@@ -14,30 +14,30 @@ import time
 import requests
 
 
-# In[136]:
+# In[55]:
 
 p3_grant_export_data = pd.read_csv("P3_GrantExport.csv", sep=";")
 p3_grant_export_data
 
 
-# In[137]:
+# In[56]:
 
 p3_grant_export_data.size
 
 
-# In[138]:
+# In[57]:
 
 # We keep only the rows which mention how much money has been granted (the amount column starts by a number)
 p3_grant_export_data = p3_grant_export_data[p3_grant_export_data['Approved Amount'].apply(lambda x : x[0].isdigit())]
 
 
-# In[139]:
+# In[58]:
 
 # Almost 200k rows have been removed
 p3_grant_export_data.size
 
 
-# In[140]:
+# In[59]:
 
 # We don't need this data
 p3_grant_export_data = p3_grant_export_data.drop(p3_grant_export_data.columns[[0]], axis = 1)
@@ -49,38 +49,84 @@ p3_grant_export_data.size
 # We will ignore all project in which the University is not mentioned : we assume that if it's not, the project is probably outside Switzerland.
 # If we have the time, a better solution would be taking the institution's location into account as well.
 
-# In[141]:
+# In[60]:
 
 # Removing rows in which University is not mentioned
 p3_grant_export_data = p3_grant_export_data.dropna(subset=['University'])
 p3_grant_export_data.size
 
 
-# In[142]:
+# In[61]:
 
 p3_grant_export_data
 
 
-# In[143]:
+# Using only university names as a parametrer for geolocators isn't enough, because we get about half of the results.
+# A better idea would be using university name, and if there is no result, using the institution name as a second chance.  
+# We will then take both universities and institution names into account in order to do our research:
+# 1) Create a key-value (name-canton) dictionary for universities and institutions :
+#     ['University', 'Canton'] and ['Institution', 'Canton']
+# 2) Go trough the dataframe:
+#   - Check if the university name exists in our index. If not, geolocate the address 
+#   - If several addresses are returned, check one that is in Switzerland (eg: University of Geneva uses to be found is US !)
+#   - If the university address is not relevant, try to find it with institution name, the same way as above
+#   - Extract the canton of the address (if it found an address and if it's in Switzerland). If no canton, let's say 'None' canton
+#   - Add the canton name to a the dictionary (or add something like 'None' if no canton has been found), so next university or institution that has been already found won't have to be geolocated again
+#   - Add the canton to a table
+# 3) Add the canton table to the above dataframe in a way that they match with the universities or institutions
+
+# In[ ]:
+
+
+
+
+# In[66]:
+
+# Let's start by creating our geolocator. We will use Google Maps API :
+googlemapsapikeyjson = json.loads(open('google_maps_api_key.json').read())
+googlemapsapikey = googlemapsapikeyjson['key']
+
+
+# In[68]:
+
+#geolocator = geopy.geocoders.GeoNames(None, geonames_login)
+geolocator = geopy.geocoders.GoogleV3(api_key=googlemapsapikey)
+test_university_geneva = geolocator.geocode("University of Geneva")
+test_university_geneva
+
+
+# That's why we have to be careful and check if an address is relevant to Switzerland !
+# So let's start by creating the indexes for 
+
+# In[ ]:
+
+
+
+
+# THE FOLLOWING CODE HAS TO BE CHECKED
+
+# In[62]:
 
 # We also delete every row that contains "Nicht zuteilbar - NA", which means that University is not mentioned.
 p3_grant_export_data = p3_grant_export_data[p3_grant_export_data.University.str.contains('Nicht zuteilbar - NA') == False]
 p3_grant_export_data.head()
 
 
-# In[144]:
+# In[63]:
 
 # The 'Approved Amount' column contains string types instead of numbers
 type(p3_grant_export_data['Approved Amount'][1])
 
 
-# In[145]:
+# In[64]:
 
 # Let's convert this column to float numbers, so we'll be able to do some maths
-p3_grant_export_data['Approved Amount'] = p3_grant_export_data['Approved Amount'].apply(float)
+# p3_grant_export_data['Approved Amount'] = p3_grant_export_data['Approved Amount'].apply(float)
+p3_grant_export_data['Approved Amount'] = p3_grant_export_data['Approved Amount'].astype(float)
+#p3_grant_export_data['Approved Amount']
 
 
-# In[146]:
+# In[65]:
 
 # Now we definitely have numbers in the 'Approved Amount' column !
 type(p3_grant_export_data['Approved Amount'][1])
@@ -89,7 +135,7 @@ type(p3_grant_export_data['Approved Amount'][1])
 # Now, time to locate universities...
 # For this, we are giong to use Geopy, which is a python client that works with most popular websites.
 
-# In[147]:
+# In[16]:
 
 json_login=open('geonames_login.json').read()
 login = json.loads(json_login)
@@ -98,7 +144,7 @@ geonames_password = login['password']
 geonames_login
 
 
-# In[148]:
+# In[17]:
 
 googlemapsapikeyjson = json.loads(open('google_maps_api_key.json').read())
 googlemapsapikey = googlemapsapikeyjson['key']
@@ -106,7 +152,7 @@ googlemapsapikey = googlemapsapikeyjson['key']
 
 # We want to locate every university, then add the corresponding canton in a new column, on the dataframe we were dealing with before.
 
-# In[149]:
+# In[18]:
 
 #geolocator = geopy.geocoders.GeoNames(None, geonames_login)
 geolocator = geopy.geocoders.GoogleV3(api_key=googlemapsapikey)
@@ -116,7 +162,7 @@ test
 
 # The only problem is that Google maps locates the University of Geneva in United States... Since we are only interested by Swiss universities, we'll add "Switzerland" at the end of each university that we'll give in parameters to the geolocator. If it finds something, we assume the University is in Switzerland, otherwise it would be outside the country.
 
-# In[150]:
+# In[19]:
 
 test = geolocator.geocode("University of Geneva Switzerland")
 test
@@ -126,19 +172,21 @@ test
 
 # We'll create a table containing all the cantons corresponding to the Universities, then we'll add this new table at the end of our dataframe. So each row will be linked to a canton.
 
-# In[151]:
+# In[20]:
 
 # Let's count the number of distinct universities we have
 p3_grant_export_data.groupby('University').Institution.nunique().size
 
 
-# In[152]:
+# THE FOLLOWING CODE HAS TO BE CHECKED
+
+# In[21]:
 
 # So we will have to make about 77 request to Geonames, which isn't that much !
 # We'll create a dataframe that will link each university to a canton.
 
 
-# In[153]:
+# In[22]:
 
 university_canton_df = p3_grant_export_data.groupby('University').Institution.nunique()
 university_canton_df = pd.DataFrame(university_canton_df)
@@ -153,13 +201,13 @@ university_canton_df.head()
 
 # TODO  : we need to remove the acronym at the end of each row, maybe it might help getting more results with geocodes.
 
-# In[154]:
+# In[23]:
 
 university_canton_df['UniversityName'] = university_canton_df['UniversityName'].apply(lambda x: x.split(' -')[0])
-university_canton_df
+university_canton_df.head()
 
 
-# In[155]:
+# In[ ]:
 
 req = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address=EPF+Lausanne+-+EPFL&key=AIzaSyBHiguwVkCbbYPAy6c0ACQTrz73JvFz4PM")
 json_response = req.json()
@@ -169,12 +217,12 @@ for i in address_components:
         print(i["long_name"] + " " + i["short_name"] + "")
 
 
-# In[158]:
+# In[ ]:
 
 university_canton_df.UniversityName
 
 
-# In[170]:
+# In[ ]:
 
 cantons_longname_table = []
 cantons_shortname_table = []
@@ -215,12 +263,12 @@ for i in university_canton_df.UniversityName:
     
 
 
-# In[161]:
+# In[ ]:
 
 len(cantons_longname_table)
 
 
-# In[83]:
+# In[ ]:
 
 cantons_table = []
 for i in university_canton_df.UniversityName:
@@ -229,7 +277,7 @@ for i in university_canton_df.UniversityName:
     cantons_table.append(canton)
 
 
-# In[94]:
+# In[ ]:
 
 cantons_table = []
 for i in university_canton_df.UniversityName:
@@ -238,7 +286,7 @@ for i in university_canton_df.UniversityName:
     cantons_table.append(canton)
 
 
-# In[95]:
+# In[ ]:
 
 cantons_table = []
 for i in university_canton_df.UniversityName:
@@ -247,32 +295,32 @@ for i in university_canton_df.UniversityName:
     cantons_table.append(canton)
 
 
-# In[84]:
+# In[ ]:
 
 cantons_table
 
 
-# In[86]:
+# In[ ]:
 
 cantons_table[0]
 
 
-# In[87]:
+# In[ ]:
 
 print(cantons_table[0])
 
 
-# In[92]:
+# In[ ]:
 
 cantons_table[0].latitude
 
 
-# In[89]:
+# In[ ]:
 
 cantons_table[0].longitude
 
 
-# In[90]:
+# In[ ]:
 
 geolocator.reverse(cantons_table[0].longitude, cantons_table[0].latitude)
 
