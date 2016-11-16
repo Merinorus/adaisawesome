c7656c137e5d54efb2cf4feedf0a063644ea26c3
merinorus <merinorus.antoine@gmail.com>
2016-11-16 13:46:10 +0100

    Before disaggregating data

--- Homework/04 - Applied ML/Homework04_Referees_teamawesome.py
+++ Homework/04 - Applied ML/Homework04_Referees_teamawesome.py
@@ -3,7 +3,7 @@
 
 # # I. Setting up the Problem
 
-# In[1]:
+# In[2]:
 
 import pandas as pd
 import numpy as np
@@ -12,7 +12,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier 
 
 
-# In[15]:
+# In[3]:
 
 filename ="CrowdstormingDataJuly1st.csv"
 Data = pd.read_csv(filename)
@@ -20,30 +20,21 @@ Data = pd.read_csv(filename)
 
 # ### 1) Peeking into the Data
 
-# In[16]:
+# In[4]:
 
 Data.ix[:10,:13]
 
 
-# In[49]:
+# In[5]:
 
 #Data.ix[:10,13:28]
-
-
-# ### 2) Disaggregate the data so each row is 1 game
-
-# In[55]:
-
-for i in Data.iterrows():
-    if Data.at[i, 'games'] > 1:
-        df
 
 
 # # II. Preparing the training & test data
 
 # ### 1) Only use players that have a Rater Image
 
-# In[38]:
+# In[6]:
 
 # 1) Remove the players without rater 1 / 2 rating because we won't be 
 # able to train or test the values (this can be done as bonus later)
@@ -52,7 +43,35 @@ Data_hasImage = Data[pd.notnull(Data['photoID'])]
 #Data_hasImage.ix[:10,13:28]
 
 
-# ### 2) Create the Training and Testing Datframes with only select data
+# 
+
+# In[ ]:
+
+
+
+
+# ### 2) Disaggregate the data so each row is 1 game
+
+# Got a lot of help from this script ! https://osf.io/w7tds/
+# It will be much simpler for us to train our random forest if each row corresponds to one game. This way, we won't have to give a different "weight" to each row according to the number of played games.
+
+# But let's start by doing the mean value of rater1 and rater 2, because if we keep them separated we might get some strange results.
+# Indeed, what if for a player, rater1 = 0.0 and rater2 = 0.75 ?
+# It would not make a lot of sense, or at least we would know our model is not viable !
+
+# In[16]:
+
+Data_hasImage['mean_rater']=(Data_hasImage['rater1']+Data_hasImage['rater2'])/2
+
+
+# Let's now disaggregate the games:
+
+# In[ ]:
+
+
+
+
+# ### 3) Create the Training and Testing Datframes with only select data
 
 # In[47]:
 
