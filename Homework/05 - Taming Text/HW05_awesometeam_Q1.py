95a361de4ef19aa10d469ce63498b39f6ef249c9
Ekaterina Paramonova <paramoed@users.noreply.github.com>
2016-12-14 15:38:13 +0100

    q1 new

--- Homework/05 - Taming Text/HW05_awesometeam_Q1.py
+++ Homework/05 - Taming Text/HW05_awesometeam_Q1.py
@@ -10,7 +10,7 @@
 
 # Generate a word cloud based on the raw corpus
 
-# In[47]:
+# In[285]:
 
 import pandas as pd
 import numpy as np
@@ -20,27 +20,25 @@ data = pd.read_csv('hillary-clinton-emails/emails.csv')
 data.ix[:,:8].head()
 
 
-# In[48]:
+# In[125]:
 
 data.ix[:,8:].head()
 
 
-# ### Trial 1) - Rawy Extracted Subject and ExtractedBodyText 
-
 # Let's only keep the text which is relevant for the raw body corpus, which is: ExtractedSubject and ExtractedBodyText, which encompass most of what the message should be about.
 
-# In[49]:
+# In[286]:
 
 data_cut = data[['ExtractedSubject','ExtractedBodyText']].copy()
 data_cut
 
 
-# In[50]:
+# In[287]:
 
 data_csv = data_cut.to_csv()
 
 
-# In[51]:
+# In[167]:
 
 # Credit to - https://github.com/amueller/word_cloud/blob/master/examples/simple.py
 
@@ -57,7 +55,7 @@ display(Image('image_raw.png'))
 
 # a) implement a standard text pre-processing pipeline (e.g., tokenization, stopword removal, stemming, etc.)
 
-# In[52]:
+# In[168]:
 
 import re
 import nltk
@@ -65,43 +63,83 @@ import nltk
 
 # ## a) Tokenization
 
-# The pirpose of tokenization is to chop up long strings into individual words or symbols. This allows for further processing of the words.
+# The purpose of tokenization is to chop up long strings into individual words or symbols. This allows for further processing of the words.
 
-# In[67]:
+# Let's first put all of the values into 1 column called "Text" and get rid of the NaNs. At this point, we do not care to distinguish between the content in the subject and the body.
 
-from nltk import word_tokenize
-data_clean = data_cut.replace(np.nan,' ', regex=True)
+# In[288]:
+
+for i in data_cut['ExtractedBodyText']:
+    data_cut.loc[len(data_cut)] = [i, 'nan']
 
 
-# In[55]:
+# In[308]:
 
-data_clean['tokenized_subj'] = data_clean.apply(lambda row: nltk.word_tokenize(row['ExtractedSubject']), axis=1)
-data_clean['tokenized_sents'] = data_clean.apply(lambda row: nltk.word_tokenize(row['ExtractedBodyText']), axis=1)
+data_clean = data_cut.drop('ExtractedBodyText', axis =1)
+data_clean.columns = ['Text']
+data_clean = data_clean[pd.notnull(data_clean['Text'])]
 data_clean
 
 
-# You can now see that the sentences / subject lines are broken up into words (or symbols) within a list, which we can now use to check for stopworkds.
+# ### Side-note: Removing capitalization
+
+# Convert all of the strings to lowercase while they are whole, as we'll need it later on.
+
+# In[309]:
+
+for index, row in data_clean.iterrows():
+    row['Text'] = row['Text'].lower()
+data_clean.head()
+
+
+# Now let's do some tokenization.
+
+# ### Tokenization
+
+# In[306]:
+
+from nltk import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+tokenizer = RegexpTokenizer(r'\w+')
+
+
+# In[310]:
+
+data_tokenized = data_clean
+for index, row in data_tokenized.iterrows():
+    row['Text'] = tokenizer.tokenize(row['Text'])
+data_tokenized.columns = ['TokenizedText']
+data_tokenized.reset_index(drop=True, inplace=True)
+data_tokenized.head()
+
+
+# You can now see that the sentences / subject lines are broken up into words within a list, which we can now use to check for stopworkds.
 
 # ## b) Removing Stopwords 
 
 # Let's see what are the stopwords that are in the nltk database that we can remove. 
 
-# In[56]:
+# In[386]:
 
 from nltk.corpus import stopwords # Import the stop word list
 stop = stopwords.words('english')
+
+
+# We will also add "fw", "fwd", "fvv", "re", "pm", and "am" to the stopwords.txt list as they are not helpful and in this context are stopwords.
+
+# In[387]:
+
 print(stopwords.words("english") )
 
 
 # As a reference to check whether the stopwords have been removed is line 15, where there is "your".
 
-# In[57]:
+# In[388]:
 
 # Remove stop words from "words"
-data_clean2 = data_clean
-data_clean2['tokenized_subj'] = data_clean2['tokenized_subj'].apply(lambda x: [item for item in x if item not in stop])
-data_clean2['tokenized_sents'] = data_clean2['tokenized_sents'].apply(lambda x: [item for item in x if item not in stop])
-data_clean2
+data_no_stop = data_tokenized
+data_no_stop['TokenizedText'] = data_no_stop['TokenizedText'].apply(lambda x: [item for item in x if item not in stop])
+data_no_stop
 
 
 # You can see that the stopword in line 15 is now gone, so we are set!
@@ -110,42 +148,32 @@ data_clean2
 
 # We need to change the words into more standard forms to reduce the inflectual forms.
 
-# In[66]:
+# In[389]:
 
-data_clean3 = data_clean2
+data_stemming = data_no_stop
 from nltk.stem.snowball import SnowballStemmer
 stemmer = SnowballStemmer("english")
 
 
-# In[65]:
+# In[390]:
 
-for w in data_clean3[["tokenized_subj"]]:
-    stemmer.stem(w)
-    
-for w in data_clean3[["tokenized_sents"]]:
-    stemmer.stem(w)
+for index, row in data_stemming.iterrows():
+    for i in row['TokenizedText']:
+        i = stemmer.stem(i)
 
-data_clean3
+data_stemming
 
 
 # ### d) generate a new word cloud
 
-# In[14]:
+# In[393]:
 
-type (data_csv_new)
+data_csv_new = data_stemming.to_csv()
 
-
-# In[13]:
-
-wordcloud = WordCloud().generate(data_csv_new)
-image = wordcloud.to_data_csv_newimage()
-#display(Image('image_raw.png'))
-image.show()
-
-
-# In[ ]:
-
-
+wordcloud_new = WordCloud().generate(data_csv_new)
+image_new = wordcloud_new.to_image()
+#display(Image('image_new.png'))
+image_new.show()
 
 
 # ## Discussion 
